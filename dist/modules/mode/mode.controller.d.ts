a7b3520d3c0ac6b665b839dd1dee949bf5ffc714
Fedor <f.avdeev@anketolog.ru>
2023-12-15 19:07:47 +0700

    update

--- dist/modules/mode/mode.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ModeService } from './mode.service';
-export declare class ModeController {
-    private modeService;
-    constructor(modeService: ModeService);
-    getMode(res: any, idUser: any): Promise<any>;
-    setMode(res: any, idUser: any, mode: any): Promise<any>;
-}
