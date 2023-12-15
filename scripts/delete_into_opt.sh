2a925a35c0999187b996f32e9ba67967aba004f2
Fedor <f.avdeev@anketolog.ru>
2023-12-15 21:10:23 +0700

    update

--- scripts/delete_into_opt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Запуск ноды
-/home/ubuntu/.nvm/versions/node/v16.18.1/bin/node delete_into_opt.js
--- scripts/update_subscription.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Запуск ноды
-/home/ubuntu/.nvm/versions/node/v16.18.1/bin/node update_subscription.js
