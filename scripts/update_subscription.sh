8c7358c90cab2b40d047d06077f9b15832193e36
Fedor <f.avdeev@anketolog.ru>
2023-12-15 19:42:58 +0700

    tests

--- scripts/update_subscription.sh
+++ scripts/update_subscription.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
 
 # Путь к базе данных SQLite
-DATABASE_PATH="/путь/к/твоей/базе/данных.db"
+DATABASE_PATH="../prisma/dev.db"
 
-# Путь до папки backups
-BACKUP_DIR="/путь/к/папке/backups"
+# SQL-запрос, который нужно выполнить
+SQL_QUERY="SELECT * FROM user;"
 
-# Проверяем количество существующих backup-файлов
-NUM_BACKUPS=0
-
-for BACKUP in $(ls $BACKUP_DIR)
-do
-    ((NUM_BACKUPS=NUM_BACKUPS+1))
-done
-
-# Если файлов больше или равно 10, то удаляем самый старый (с наименьшим номером)
-if ((NUM_BACKUPS >= 10))
-then
-    MIN_FILE_NUM="10"
-    MIN_FILE_NAME=""
-
-    for BACKUP in $(ls $BACKUP_DIR)
-    do
-        FILE_NUM=${BACKUP%.*}
-        if [[ "$FILE_NUM" -lt "$MIN_FILE_NUM" ]]
-        then
-            MIN_FILE_NUM=$FILE_NUM
-            MIN_FILE_NAME=$BACKUP
-        fi
-    done
-    rm "$BACKUP_DIR/$MIN_FILE_NAME"
-fi
-
-# После того как уверились, что в папке не более 10 файлов, создаем новый
-NEW_BACKUP="$BACKUP_DIR/$((NUM_BACKUPS+1)).db"
-cp $DATABASE_PATH $NEW_BACKUP
+# Выполняем SQL-запрос с использованием sqlite3
+RESULT=$(sqlite3 "$DATABASE_PATH" "$SQL_QUERY")
