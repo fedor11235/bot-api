18e464e4d19f0eed823bfe4d83fb5522b31725de
mr_smile <yaroslav.efrem@yandex.ru>
2024-01-16 12:55:34 +0300

    refactoring code

--- scripts/delete_into_opt.sh
+++ scripts/delete_into_opt.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
-filepath="../prisma/dev.db"
+
+filepath="../prisma/test.db"
+
 createDbConnection() {
   sqlite3 "$filepath" <<EOF
 EOF
   echo "Connection with SQLite has been established"
 }
+
 db_each() {
   while IFS="|" read -r id booking_date view; do
     if [ -n "$booking_date" ]; then
-      bookingDateArray=($(echo "$booking_date" | tr -d '_' | tr -d ' '))
-      maxValue=0
+      IFS="_" read -r -a bookingDateArray <<< "$booking_date"
+      maxValue="0.0"
+      elementArray=()
       for element in "${bookingDateArray[@]}"; do
-        array=($(echo "$element" | tr -d '/' | tr -d ''))
-        value=$(echo "${array[1]}.${array[0]}" | bc)
-        if (( $(echo "$value > $maxValue" | bc -l) )); then
-          maxValue=$value
+        IFS="/" read -r -a elementArray <<< "$element"
+        date="${elementArray[1]}"
+
+        IFS="." read -r dateElementDay dateElementMonth <<< "$date"
+        let dateElementDayInt="$dateElementDay"
+        let dateElementMonthInt="$dateElementMonth"
+
+        IFS="." read -r dateMaxDay dateMaxMonth <<< "$maxValue"
+        let dateMaxDayInt="$dateMaxDay"
+        let dateMaxMonthInt="$dateMaxMonth"
+
+        if [[ ($dateMaxMonthInt < $dateElementMonthInt)  ||
+              (($dateMaxMonthInt -eq $dateElementMonthInt) && ($dateMaxDayInt < $dateElementDayInt)) ]]; then
+          maxValue=$date
         fi
+        elementArray=()
       done
-      prepareDate=$(date -d "@$maxValue" +"%Y-%m-%d")
-      dateNow=$(date +"%s")
-      if (( maxValue < dateNow )); then
-        sqlite3 "$filepath" "UPDATE recommendationInto SET view = 'false' WHERE id = $id"
-        echo $id
-        echo "Row $id has been updated"
+
+      # на данном этапе нашли максимальную дату и теперь сравниваем её с текущей
+      IFS="." read -r dateMaxDay dateMaxMonth <<< "$maxValue"
+      let dateMaxDayInt="$dateMaxDay"
+      let dateMaxMonthInt="$dateMaxMonth"
+      # в коде выше получили числовые представления дня и месяца максимальной даты
+
+      dateNow=$(date +"%d.%m") # получаем текущую даты в формате <день>.<месяц>
+      IFS="." read -r dateNowDay dateNowMonth <<< "$dateNow"
+      let dateNowDayInt="$dateNowDay"
+      let dateNowMonthInt="$dateNowMonth"
+      # в коде выше получили числовые представления дня и месяца текущей даты
+
+      # ниже в условии просто проверяем, что текущая дата больше максимальной; и если это так, то апдейтим таблицу по id
+      if [[ ($dateMaxMonthInt < $dateNowMonthInt)  ||
+      (($dateMaxMonthInt -eq $dateNowMonthInt) && ($dateMaxDayInt < $dateNowDayInt)) ]]; then
+       sqlite3 "$filepath" "UPDATE recommendationInto SET view = false WHERE id = $id"
+       echo "Row $id has been updated"
       fi
     fi
   done < <(sqlite3 -separator "|" "$filepath" "SELECT id, booking_date, view FROM recommendationInto")
